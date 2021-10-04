50d1a6eaf09fe413b302842eda13bfc9053dfd52
Louis Young <1462648167>
2021-10-05 02:34:06 +0800

    fix(server.url): remove extra string 'lock'

--- Server/url.py
+++ Server/url.py
@@ -4,7 +4,7 @@ def urls(url, request):
     if (url == "/log"): return log(request)
     elif (url == "/system-info"): return systemInfo(request)
     elif (url == "/config"): return serverConfig(request)
-    elif (url == "/lock-status"): return lockStatus(request)lock
+    elif (url == "/lock-status"): return lockStatus(request)
     elif (url == "/lock"): return lock(request)
     elif (url == "/unlock"): return unlock(request)
     elif (url == "/fallback"): return fallback(request)
